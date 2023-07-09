77fd588c9a10187b65e892b1da426a5e0242c286
Luismmpg <luis.mmp.goncalves@gmail.com>
2023-07-09 19:11:00 +0100

    Adicionada função para desistir

--- app.py
+++ app.py
@@ -5,24 +5,19 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "segredo_nao_me_contes"
 
 respostas = (
-    "resposta0",
-    "resposta1",
-    "resposta2",
-    "resposta3",
-    "resposta4",
-    "resposta5",
+    "avestruz",
+    "sete",
+    "seis",
+    "lua",
+    "h2o",
 )
-mensagens = []
+
+# mensagens = []
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     form = RespostaForm()
-    if form.validate_on_submit():
-        if form.resposta.data.lower() == respostas[0]:
-            return redirect(url_for("pergunta1"))
-        else:
-            flash("Resposta incorreta. Tenta novamente.", "error")
     return render_template("home.html", form=form)
 
 
@@ -30,7 +25,7 @@ def home():
 def pergunta1():
     form = RespostaForm()
     if form.validate_on_submit():
-        if form.resposta.data.lower() == respostas[1]:
+        if form.resposta.data.lower() == respostas[0]:
             return redirect(url_for("pergunta2"))
         else:
             flash("Resposta incorreta. Tenta novamente.", "error")
@@ -41,7 +36,7 @@ def pergunta1():
 def pergunta2():
     form = RespostaForm()
     if form.validate_on_submit():
-        if form.resposta.data.lower() == respostas[2]:
+        if form.resposta.data.lower() == respostas[1]:
             return redirect(url_for("pergunta3"))
         else:
             flash("Resposta incorreta. Tenta novamente.", "error")
@@ -52,30 +47,58 @@ def pergunta2():
 def pergunta3():
     form = RespostaForm()
     if form.validate_on_submit():
-        if form.resposta.data.lower() == respostas[3]:
+        if form.resposta.data.lower() == respostas[2]:
             return redirect(url_for("pergunta4"))
         else:
             flash("Resposta incorreta. Tenta novamente.", "error")
     return render_template("pergunta3.html", form=form)
 
 
-@app.route("/tesouro", methods=["GET", "POST"])
-def tesouro():
-    form = MensagemForm()
+@app.route("/pergunta4", methods=["GET", "POST"])
+def pergunta4():
+    form = RespostaForm()
     if form.validate_on_submit():
-        mensagem = form.mensagem.data
-        mensagens.append(mensagem)
-        print(f"Mensagem recebida: {mensagem}")  # Print da mensagem no terminal
-        flash(
-            f"Mensagem enviada com sucesso! Já existem {len(mensagens)} mensagem(s).",
-            "success",
-        )
-    return render_template("tesouro.html", form=form)
+        if form.resposta.data.lower() == respostas[3]:
+            return redirect(url_for("pergunta5"))
+        else:
+            flash("Resposta incorreta. Tenta novamente.", "error")
+    return render_template("pergunta4.html", form=form)
 
 
-@app.route("/mensagens")
-def ver_mensagens():
-    return render_template("mensagens.html", mensagens=mensagens)
+@app.route("/pergunta5", methods=["GET", "POST"])
+def pergunta5():
+    form = RespostaForm()
+    if form.validate_on_submit():
+        if form.resposta.data.lower() == respostas[4]:
+            return redirect(url_for("vitoria"))
+        else:
+            flash("Resposta incorreta. Tenta novamente.", "error")
+    return render_template("pergunta5.html", form=form)
+
+
+@app.route("/vitoria", methods=["GET", "POST"])
+def vitoria():
+    form = MensagemForm()
+    # if form.validate_on_submit():
+    #     mensagem = form.mensagem.data
+    #     mensagens.append(mensagem)
+    #     print(f"Mensagem recebida: {mensagem}")
+    #     flash(
+    #         f"Mensagem enviada com sucesso! Já existem {len(mensagens)} mensagem(s).",
+    #         "success",
+    #     )
+    return render_template("vitoria.html", form=form)
+
+
+@app.route("/desistir", methods=["GET", "POST"])
+def desistir():
+    form = RespostaForm()
+    return render_template("desistir.html", form=form)
+
+
+# @app.route("/mensagens")
+# def ver_mensagens():
+#     return render_template("mensagens.html", mensagens=mensagens)
 
 
 if __name__ == "__main__":
